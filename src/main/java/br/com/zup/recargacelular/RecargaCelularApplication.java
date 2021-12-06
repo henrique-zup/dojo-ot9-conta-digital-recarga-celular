d67fc0209add08768d1a78a64a2b0acd196f8f16
Henrique César <henrique.jose@zup.com.br>
2021-12-06 16:47:21 -0300

    setup do projeto

---
+++ src/main/java/br/com/zup/recargacelular/RecargaCelularApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.recargacelular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecargaCelularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecargaCelularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/recargacelular/RecargaCelularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.recargacelular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecargaCelularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
